022afcb9d3766cddca9852c7b02f36655ddedc18
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2024-07-04 12:46:59 +0900

    folder scaffolding

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <section>header here</section>
+    <slot />
+    <section>footer here</section>
+    <!-- https://nuxt.com/docs/guide/directory-structure/layouts -->
+    <!-- layouts/desktop/default.vue -->
+    <!-- layouts/tablet/... -->
+    <!-- layouts/mobile/... -->
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <UButton label="btn" />
+  </div>
+</template>
